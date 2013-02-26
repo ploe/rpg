96d9981b5ee1e06f9dfaa771445b66b21e4fb260
Mika Attila <radiantstatue@gmail.com>
2013-02-26 13:31:15 +0100

    main.lua: Replace tabs by spaces

--- main.lua
+++ main.lua
@@ -11,12 +11,12 @@ xOff = 0
 yOff = 0
 
 function love.load()
-	JIFFY = 1/30
-	Action:push(Player)
-	if not Map.load("plains.lua") then
-		print('Failed to load map')
-		love.event.quit()
-    	end
+    JIFFY = 1/30
+    Action:push(Player)
+    if not Map.load("plains.lua") then
+        print('Failed to load map')
+        love.event.quit()
+        end
     
     Editor.init()
     currentState = Game
@@ -24,28 +24,28 @@ function love.load()
 end
 
 function love.update()
-	start = love.timer.getMicroTime()
-	if currentState.update then currentState.update() end
+    start = love.timer.getMicroTime()
+    if currentState.update then currentState.update() end
 end
 
 function love.keypressed(key, unicode)
-	if key == 'return' and love.keyboard.isDown('lalt') then
-		love.graphics.toggleFullscreen()
-	end
-	if currentState.keypressed then currentState.keypressed(key, unicode) end
+    if key == 'return' and love.keyboard.isDown('lalt') then
+        love.graphics.toggleFullscreen()
+    end
+    if currentState.keypressed then currentState.keypressed(key, unicode) end
 end
 
 function love.mousepressed(x, y, button)
-	if currentState.mousepressed then currentState.mousepressed(x, y, button) end
+    if currentState.mousepressed then currentState.mousepressed(x, y, button) end
 end
 
 function love.mousereleased(x, y, button)
-	if currentState.mousereleased then currentState.mousereleased(x, y, button) end
+    if currentState.mousereleased then currentState.mousereleased(x, y, button) end
 end
 
 function love.draw()
-	if currentState.draw then currentState.draw() end
-	if love.timer.getMicroTime() <= start + JIFFY then love.timer.sleep(start + JIFFY - love.timer.getMicroTime()) end
-	Signal = {}
+    if currentState.draw then currentState.draw() end
+    if love.timer.getMicroTime() <= start + JIFFY then love.timer.sleep(start + JIFFY - love.timer.getMicroTime()) end
+    Signal = {}
 end
 
