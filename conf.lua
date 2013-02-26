c2edc7afe52417308a5b380bf1f7165410ce3dba
Mika Attila <radiantstatue@gmail.com>
2013-02-26 14:12:19 +0100

    Add conf.lua

---
+++ conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    -- Some meta data
+    t.author = "Ploe and SneakySnake"
+    t.url = "http://forum.moosader.info/viewtopic.php?f=91&t=2650"
+    -- Game was made for LOVE 0.8.0
+    t.version = "0.8.0"
+    -- Initial window title
+    t.title = "RPG"
+    -- Disable unneeded modules
+    t.modules.joystick = false
+    t.modules.physics = false
+end
